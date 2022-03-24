7cc599a8d4fdf5f1e3e33f57cdae4dd68502b19a
Caden <Cpchambers01@gmail.com>
2022-03-24 18:58:16 -0400

    fixed git issues and basic nav working -Caden

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app";
+import App from "./App";
 
 const app = document.getElementById("app");
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  app
 );
