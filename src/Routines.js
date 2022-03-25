96181674aa2b8a3a23cffd6b82da76e88f661ea9
Caden <Cpchambers01@gmail.com>
2022-03-24 21:03:20 -0400

    basic routines.js built -caden

--- src/Routines.js
+++ src/Routines.js
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from "react";
+
 const Routines = () => {
+  const [routines, setRoutines] = useState([]);
+
+  const fetchRoutine = async () => {
+    const routines = await fetch(
+      "http://fitnesstrac-kr.herokuapp.com/api/routines",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const info = await routines.json();
+    console.log(info);
+    setRoutines(info);
+  };
+
+  useEffect(() => {
+    fetchRoutine();
+  }, []);
+
   return (
     <>
       <h1>Routines</h1>
+      <div>
+        {routines.map((routine) => {
+          return (
+            <div key={routine.id}>
+              <h1>{routine.name}</h1>
+              <h4>{routine.goal}</h4>
+              <h4>{routine.creatorName}</h4>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
