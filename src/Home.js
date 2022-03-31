02f5da9f7ac4b493a245a40d5a2f4ac0297fe8bb
micca1 <mjcastranova@gmail.com>
2022-03-31 18:41:37 -0400

    d

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 const Home = () => {
   return (
     <>
-      <br></br>
-      <br></br>
       <h2>Welcome to Your Custom Fitness Tracker Experience</h2>
-      <h3>Record Your Journey to Fit</h3>
+      <h3>Your Journey to Fit</h3>
+      {/* <FontAwesomeIcon icon="fa-solid fa-person-running" /> */}
+      {/* <i class="fa-solid fa-person-running"></i> */}
       <div class="steps">
-        <h4>Step 1: If you are a new user, first set up you account below</h4>
+        <h4>Step 1: If you are a new user, set up your account below</h4>
         <button class="glow-on-hover" type="button">
           Create Account
         </button>
@@ -14,11 +14,28 @@ const Home = () => {
           Step 2: Once you have created an account check out our database of
           activities and routines.
         </h4>
+        <div class="click">
+          <button class="glow-on-hover" type="button">
+            Activities
+          </button>
+          <br></br>
+          <br></br>
+          <button class="glow-on-hover" type="button">
+            Routines
+          </button>
+        </div>
         <h4 span class="create">
-          Didn't see one you liked? Create your own
-          <a href="http://localhost:3000/Routines"> here</a>
+          <em>
+            Didn't see one you liked? Create your own
+            <a href="http://localhost:3000/Routines"> here</a>
+          </em>
+        </h4>
+        <h4>
+          Step 3: Stay Consistent, Stay Hungry, and Build the Best Version of
+          You!
         </h4>
       </div>
+      {/* </section> */}
     </>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,25 +3,26 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div id="nav">
-        <div id="navtitle">
+      <section class="heroimage">
+        <div id="nav">
           <h1>Fitness Tracker</h1>
+
+          <div id="links">
+            <Link to="/" className="link">
+              Hello
+            </Link>
+            <Link to="/Routines" className="link">
+              Routines
+            </Link>
+            <Link to="/Activities" className="link">
+              Activities
+            </Link>
+            <Link to="/Login" className="link">
+              Login/Register
+            </Link>
+          </div>
         </div>
-        <div id="links">
-          <Link to="/" className="link">
-            Hello
-          </Link>
-          <Link to="/Routines" className="link">
-            Routines
-          </Link>
-          <Link to="/Activities" className="link">
-            Activities
-          </Link>
-          <Link to="/Login" className="link">
-            Login/Register
-          </Link>
-        </div>
-      </div>
+      </section>
     </>
   );
 };
